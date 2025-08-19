c4705045218c4c4e4783afd6cfd4924df207fc2b
S1rt3ge <buryanov.alexey@gmail.com>
2025-08-19 17:33:30 +0300

    Initial commit

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=A:\projects\bootcamp_rally_claude\.venv
+set VIRTUAL_ENV=A:\projects\bootcamp_rally\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
